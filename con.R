c2b4e55599efb3e9777957b0a159e6383a57ac03
George Baker <gbakes89@gmail.com>
2019-04-28 01:01:36 +0200

    cleaning up

--- con.R
+++
@@ -1,4 +0,0 @@
-library(RPostgres)
-con <- dbConnect(RPostgres::Postgres(), dbname = "twitter_malta",
-                 host = "46.101.159.150", user = "gbakes",
-                 password = "GhawdexMLT478")
